41f9bb65b190a9bf0fa7e806d6b562c0d0472b51
KaiiMichii <janjantagoylo@gmail.com>
2024-09-20 15:17:56 +0800

    KaiiMichii

---
+++ it20c.java
@@ -0,0 +1,24 @@
+
+package arrays;
+
+
+public class Arrays {
+
+    public static void main(String[] args) {
+        
+        char [][] kaii = {
+            
+            {'*','*','*','*',},
+              {'*','*','*','*',},
+                {'*','*','*','*',},
+                  {'*','*','*','*',},
+            
+        };
+for (int i=0; i <kaii.length; i++) {
+    System.out.println();
+    for (int j=0;j< kaii[i].length; j++);
+     System.out.println(kaii[i]);
+}
+    }
+    
+}
